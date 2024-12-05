29d0e0f135f5470170a995b4ab2f512b36261abc
puntacode <pablo.molina@gmail.com>
2024-12-05 19:18:16 -0300

    creado la vista y la clase que crea todo el crud que podamos necesitar

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+
+from rest_framework import viewsets
+from .serializer import TaskSerializer
+from .models import Task
 
 # Create your views here.
+#esto crea todo el crud que vamos a necesitar
+class TaskView(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
