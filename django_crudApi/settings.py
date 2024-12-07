c8ac8d83add42e44bee42360375faaa5e77e85db
puntacode <pablo.molina@gmail.com>
2024-12-07 11:34:11 -0300

    feature de react funcional

--- django_crudApi/settings.py
+++ django_crudApi/settings.py
@@ -128,5 +128,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #cors autorizaciones de quien puede conectarse
-CORS_ALLOWED_ORIGINS = []
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
 
